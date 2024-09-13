3b9f3c6bfb82a5f5e1fc6d4c97282c8b59bd5759
Pygc <patricia.gonzalez2001@alumnos.ubiobio.cl>
2024-09-13 02:39:36 -0300

    Condicionales

---
+++ Ejercicio4.js
@@ -0,0 +1,56 @@
+// Condicionales
+
+// Valores numéricos
+const numero1 = 10;
+const numero2 = 20;
+const numero3 = 30;
+
+// 1. Imprimir el número mayor de los tres
+const numeroMayor = Math.max(numero1, numero2, numero3);
+console.log("El número mayor es:", numeroMayor);
+
+// 2. Imprimir el número menor de los tres
+const numeroMenor = Math.min(numero1, numero2, numero3);
+console.log("El número menor es:", numeroMenor);
+
+// 3. Imprimir si numero1 es par o impar
+if (numero1 % 2 === 0) {
+  console.log("numero1 es par");
+} else {
+  console.log("numero1 es impar");
+}
+
+// 4. Imprimir si numero2 es par o impar
+if (numero2 % 2 === 0) {
+  console.log("numero2 es par");
+} else {
+  console.log("numero2 es impar");
+}
+
+// 5. Imprimir si numero3 es par o impar
+if (numero3 % 2 === 0) {
+  console.log("numero3 es par");
+} else {
+  console.log("numero3 es impar");
+}
+
+// 6. Imprimir si numero1 es múltiplo de 5
+if (numero1 % 5 === 0) {
+  console.log("numero1 es múltiplo de 5");
+} else {
+  console.log("numero1 no es múltiplo de 5");
+}
+
+// 7. Imprimir si numero2 es múltiplo de 5
+if (numero2 % 5 === 0) {
+  console.log("numero2 es múltiplo de 5");
+} else {
+  console.log("numero2 no es múltiplo de 5");
+}
+
+// 8. Imprimir si numero3 es múltiplo de 5
+if (numero3 % 5 === 0) {
+  console.log("numero3 es múltiplo de 5");
+} else {
+  console.log("numero3 no es múltiplo de 5");
+}
