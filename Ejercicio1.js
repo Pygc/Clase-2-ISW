18b448b3f14f51a7264904ef05adc790574502e5
Pygc <patricia.gonzalez2001@alumnos.ubiobio.cl>
2024-09-13 02:29:58 -0300

    Variables y tipos de datos

---
+++ Ejercicio1.js
@@ -0,0 +1,20 @@
+// Variables y Tipos de Datos
+
+// 1. Declara una variable llamada `nombre` y asígnale un valor de tipo `string`.
+let nombre = "Francisco";
+
+// 2. Declara una variable llamada `edad` y asígnale un valor de tipo `number`.
+let edad = 23;
+
+// 3. Declara una variable llamada `puedoConducir` y asígnale un valor de tipo `boolean`.
+let puedoConducir = true;
+
+// 4. Para cada variable se debe imprimir por consola el resultado de cada variable declarada.
+console.log(nombre);
+console.log(edad);
+console.log(puedoConducir);
+
+// 5. Para cada variable se debe imprimir el tipo de dato que es por consola.
+console.log(typeof nombre);
+console.log(typeof edad);
+console.log(typeof puedoConducir);
