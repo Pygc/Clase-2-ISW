1eaae31bf1a54c856d6b2753df9a0540788adff9
Pygc <patricia.gonzalez2001@alumnos.ubiobio.cl>
2024-09-13 02:36:10 -0300

    Operadores Aritméticos

---
+++ Ejercicio2.js
@@ -0,0 +1,25 @@
+// Operadores Aritméticos
+
+// Valores numéricos
+const numero1 = 20;
+const numero2 = 40;
+
+// 1. Suma (+) :  Suma numero1 y numero2. Muestra el resultado por consola.
+const suma = numero1 + numero2;
+console.log(suma); 
+
+// 2. Resta (-) : Resta numero1 y numero2. Muestra el resultado por consola.
+const resta = numero1 - numero2;
+console.log(resta); 
+
+// 3. Multiplicación (*) : Multiplica numero1 y numero2. Muestra el resultado por consola.
+const multiplicacion = numero1 * numero2;
+console.log(multiplicacion); 
+
+// 4. División (/) : Divide numero1 y numero2. Muestra el resultado por consola.
+const division = numero1 / numero2;
+console.log(division); 
+
+// 5. Módulo (%) : Saca el modulo entre numero2 y numero1. Muestra el resultado por consola.
+const modulo = numero2 % numero1;
+console.log(modulo); 
